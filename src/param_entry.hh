9ccb1cc9fbed5cc277e846521b0d384d066f6d37
Andrew Torda <torda@zbh.uni-hamburg.de>
2016-09-29 13:22:39 +0200

    Trivial reordering (clang warning)

--- src/param_entry.hh
+++ src/param_entry.hh
@@ -4,16 +4,15 @@
 #ifndef PARAM_ENTRY_HH
 #define PARAM_ENTRY_HH
 
-#ifdef __clang__  /* The param_entry gets 3 bytes of padding. Hard to avoid */
-#   pragma clang diagnostic ignored "-Wpadded"
-#endif /* __clang__ */
-/*
- * When a variable is born
-    param_entry p = 5;
+/* When a variable is born
+ *  param_entry p = 5;
  *  it invokes the constructor.
  *  When we reassign to it, p = 7;, it invokes the assignment operator.
  */
-#include <csignal>
+#ifdef __clang__  /* The param_entry gets 3 bytes of padding. Hard to avoid */
+#   pragma clang diagnostic ignored "-Wpadded"
+#endif /* __clang__ */
+
 class param_entry {
 public:
     static const unsigned char invalid_t  = 0;
@@ -32,7 +31,6 @@ private:
         bool b;
     } the_var;
     unsigned char this_t;
-    const union mixed_t i_v = {-1};
 
     void set (const int i);
     void set (const float f   )     {the_var.f = f;}
@@ -42,26 +40,27 @@ private:
     void set (const char *ss)       {set (std::string (ss));}
     void set (const std::string &ss);
 public:
-    param_entry ()                : this_t(invalid_t),  s(),   the_var(i_v) {};
-    param_entry (const int i)     : this_t(int_t),      s(),   the_var(i_v) { set(i);}
-    param_entry (const float f)   : this_t(float_t),    s(),   the_var(i_v) { set(f);}
-    param_entry (const double d)  : this_t(float_t),    s(),   the_var(i_v) { set(float(d));}
-    param_entry (const unsigned u): this_t(unsigned_t), s(),   the_var(i_v) { set(u);}
-    param_entry (const bool b)    : this_t(bool_t),     s(),   the_var(i_v) { set(b);}
-    param_entry (const char *ss)  : this_t(string_t),   s(ss), the_var(i_v) { set(ss);}
-//  param_entry (const std::string *ss) : this_t (string_t) s(ss), the_var(i_v) {set(ss);}
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weffc++"
 
-    /* If I clean this up, set should return the right type */
+    param_entry ()                : s(), this_t(invalid_t)   {}
+    param_entry (const int i)     : s(), this_t(int_t)       { set(i);}
+    param_entry (const float f)   : s(), this_t(float_t)     { set(f);}
+    param_entry (const double d)  : s(), this_t(float_t)     { set(float(d));}
+    param_entry (const unsigned u): s(), this_t(unsigned_t)  { set(u);}
+    param_entry (const bool b)    : s(), this_t(bool_t)      { set(b);}
+    param_entry (const char *ss)  : s(), this_t(string_t)    { set(ss);}
 
-    float          operator =(float  f)         {set(f);   return f;}
-    int            operator =(int    i)         {set(i);   return i;}
-    double         operator =(double d)         {set(d);   return d;}
-    unsigned       operator =(unsigned u )      {set(u);   return u;}
-    bool           operator =(bool b)           {set(b);   return b;}
-    char *         operator =(char *cp)         {set(cp);  return cp;}
-    const char *   operator =(const char *cp)   {set (cp); return cp;}
-    std::string    &operator =(std::string &ss) {set(ss);  return ss;}
-
+    float              operator =(float  f)              {set(f);  return f;}
+    int                operator =(int    i)              {set(i);  return i;}
+    double             operator =(double d)              {set(d);  return d;}
+    unsigned           operator =(unsigned u )           {set(u);  return u;}
+    bool               operator =(bool b)                {set(b);  return b;}
+    char *             operator =(char *cp)              {set(cp); return cp;}
+    const char *       operator =(const char *cp)        {set(cp); return cp;}
+    std::string       &operator =(std::string &ss)       {set(ss); return ss;}
+    const std::string &operator =(const std::string &ss) {set(ss); return ss;}
+#pragma GCC diagnostic pop
     operator int()         { return the_var.i;} /* This funny overloading of  */
     operator float()       { return the_var.f;} /* the cast operator let's us */
     operator unsigned()    { return the_var.u;} /* return different types.    */
